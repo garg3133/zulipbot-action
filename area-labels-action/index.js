296729a9e6ac2728c37ba24705bf198c49c15189
Priyansh Garg <priyanshgarg30@gmail.com>
2021-05-24 01:39:15 +0530

    Refactor area labels action index to use shared code.

--- area-labels-action/index.js
+++ area-labels-action/index.js
@@ -1,28 +1,38 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const config = require("./clientConfig.js");
-const areaLabel = require("./areaLabel.js");
+import { setFailed } from "@actions/core";
+import { context } from "@actions/github";
+import { getClient, getClientLogin } from "../client_config/client";
+import getUserConfig from "../client_config/getUserConfig";
+import * as areaLabel from "./areaLabel";
 
 const run = async () => {
-  const client = await config.getClient();
+  try {
+    const client = getClient();
+    console.log(client);
 
-  const context = github.context;
-  if (
-    context.eventName !== "issues" &&
-    context.eventName !== "pull_request_target"
-  )
-    return;
+    // Get bot's username
+    client.username = await getClientLogin(client);
+    console.log("Client username: ", client.username);
 
-  const payload = context.payload;
-  if (payload.action === "labeled" || payload.action === "unlabeled") {
-    areaLabel.run(client, payload);
+    const {owner, repo} = context.issue;
+
+    // Get user configuration
+    client.config = await getUserConfig(client, owner, repo);
+
+    if (
+      context.eventName !== "issues" &&
+      context.eventName !== "pull_request_target"
+    )
+      return;
+
+    const payload = context.payload;
+    if (payload.action === "labeled" || payload.action === "unlabeled") {
+      areaLabel.run(client, payload);
+    }
+  } catch (error) {
+    setFailed(error.message);
   }
 };
 
 
 // Run the script
-try {
-  run();
-} catch (error) {
-  core.setFailed(error.message);
-}
+run();
