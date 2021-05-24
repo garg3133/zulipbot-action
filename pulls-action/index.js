4a8c19654fd1925b8afaffe23bc35bd5635f49c5
Priyansh Garg <priyanshgarg30@gmail.com>
2021-05-24 22:03:17 +0530

    Refactor pulls-action index to use shared code.

--- pulls-action/index.js
+++ pulls-action/index.js
@@ -1,30 +1,29 @@
 import { setFailed } from "@actions/core";
 import { context } from "@actions/github";
-import * as clientConfig from "./clientConfig";
-import * as pulls from "./pulls/pulls"
+import { getClient } from "../client_config/client";
+import getUserConfig from "../client_config/getUserConfig";
+import * as pulls from "./pulls/pulls";
 import * as workflow_run from "./workflow_run/workflow_run";
 
 const run = async () => {
-  const client = await clientConfig.getClient();
+  try {
+    const client = getClient();
 
-  const payload = context.payload;
+    const { owner, repo } = context.issue;
+    const payload = context.payload;
 
-  const { owner, repo } = context.issue;
+    if (context.eventName === "pull_request") {
+      // Get user configuration
+      client.config = await getUserConfig(client, owner, repo);
 
-  if (context.eventName === "pull_request") {
-    // Get action's configuration
-    client.config = await clientConfig.getActionConfig(client, owner, repo);
-
-    pulls.run(client, payload);
-  } else if (context.eventName === "workflow_run") {
-    workflow_run.run(client, payload, owner, repo);
+      pulls.run(client, payload);
+    } else if (context.eventName === "workflow_run") {
+      workflow_run.run(client, payload, owner, repo);
+    }
+  } catch (error) {
+    setFailed(error.message);
   }
-
 };
 
 // Run the script
-try {
-  run();
-} catch (error) {
-  setFailed(error.message);
-}
+run();
