f0df66f05bcfdcc760c4204f023bb8d0a2890c47
Priyansh Garg <priyanshgarg30@gmail.com>
2021-06-05 19:29:19 +0530

    Remove unnecessary types and interfaces from global.

--- interfaces.ts
+++ interfaces.ts
@@ -1,11 +1,3 @@
-export interface CommandsActionConfigInterface {
-  claim?:
-    | {
-        max_assignees?: number;
-      }
-    | boolean;
-}
-
 export interface AreasActionConfigInterface {
   area_labels?: {
     [key: string]: string;
--- types.ts
+++ types.ts
@@ -1,28 +1,3 @@
-import Template from "./structures/Template";
 import { GitHub } from "@actions/github/lib/utils";
-import { IssueCommentCreatedEvent } from "@octokit/webhooks-types";
-import {
-  AreasActionConfigInterface,
-  CommandsActionConfigInterface,
-} from "./interfaces";
-
-export type UserConfig =
-  | CommandsActionConfigInterface
-  | AreasActionConfigInterface;
-
-export type Client = InstanceType<typeof GitHub> & {
-  username?: string;
-  config?: UserConfig;
-  commands?: Map<string, CommandRunFunction>;
-  templates?: Map<string, Template>;
-};
 
 export type OctokitClient = InstanceType<typeof GitHub>;
-
-export type CommandRunFunction = (
-  client: Client,
-  payload: IssueCommentCreatedEvent,
-  args: string,
-  owner: string,
-  repo: string
-) => Promise<void>;
