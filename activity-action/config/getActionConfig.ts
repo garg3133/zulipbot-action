ed7b3215ffa6ce298f9a4e8e7a260a69c7905331
Priyansh Garg <priyanshgarg30@gmail.com>
2021-07-29 02:30:13 +0530

    AcA: Remove unnecessary configs from activity-action.

--- activity-action/config/getActionConfig.ts
+++ activity-action/config/getActionConfig.ts
@@ -3,13 +3,10 @@ import { ActivityActionUserConfigInterface } from "../interfaces";
 
 export default function getActionConfig(): ActivityActionUserConfigInterface {
   const config: ActivityActionUserConfigInterface = {
-    issue_assigned_label: getInput("issue_assigned_label"),
     skip_issue_with_label: getInput("skip_issue_with_label"),
     skip_issue_with_pull_label: getInput("skip_issue_with_pull_label"),
-    clear_closed_issue: getInput("clear_closed_issue") === "true",
     days_until_warning: parseInt(getInput("days_until_warning")),
     days_until_unassign: parseInt(getInput("days_until_unassign")),
-    assign_pull_to_reviewer: getInput("assign_pull_to_reviewer") === "true",
   };
 
   console.log("Config:", config);
--- activity-action/interfaces.ts
+++ activity-action/interfaces.ts
@@ -1,9 +1,6 @@
 export interface ActivityActionUserConfigInterface {
-  issue_assigned_label: string;
   skip_issue_with_label: string;
   skip_issue_with_pull_label: string;
-  clear_closed_issue: boolean;
   days_until_warning: number;
   days_until_unassign: number;
-  assign_pull_to_reviewer: boolean;
 }
